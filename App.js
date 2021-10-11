2b5e1fd9494c5d16411b47805075c81bfc4c0574
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-11 16:04:49 +0900

    Layout System

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View></View>
-      <View></View>
-      <View></View>
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
