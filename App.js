8983255da7f52a6140ccaa239297eaa5e2c2f890
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-13 14:03:05 +0900

    #2.5 Styles

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.des}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#595959",
+  },
+  // City
+  city: {
+    flex: 1.1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "white",
+    fontSize: 60,
+    fontWeight: "700",
+  },
+  // Weather
+  weather: {
+    flex: 3.5,
+  },
+  day: {
+    flex: 1,
+
+    alignItems: "center",
+  },
+  temp: {
+    color: "white",
+    marginTop: 50,
+    fontSize: 160,
+  },
+  des: {
+    color: "white",
+    marginTop: -20,
+    fontSize: 40,
+  },
+});
